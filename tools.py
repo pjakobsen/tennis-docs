2c06fa995f5126e0ceb2c4921bec150dfb2290a6
pjakobsen <pjakobsen@gmail.com>
2013-06-20 12:40:48 -0400

    Now writing json data store from Md file

--- tools.py
+++ tools.py
@@ -1,3 +1,6 @@
+import json
+from pprint import pprint
+
 def dict_from_courses():
     # 
     '''
@@ -12,13 +15,37 @@ def dict_from_courses():
     '''
     file = open('sot.md')
     lessons=[]
+    month = 1
+    type = 'lesson'
+    id = 1
+    
     for line in file:
-        month = 1
+        stroke='all'
+        if not line:continue
+        if "CLASSES" in line:continue
+        if "Month " in line: continue
+        if "Month " in line: month = line.split("Month ")[1].split(":")[0]
+        elif "Lecture" in line: type=="lecture"
+        else:
+            lecture_number = line.split(" ")[0]
+            title = line[2:].strip()
         print line
-        if "Month " in title: month = line.split("Month ")[1]
+        if "orehand" in title: stroke = "forehand"
+        elif "ackhand" in title: stroke = "backhand"
+        elif "olley" in title: stroke = "volley"
+        elif "erve " in title: stroke = "serve"
+        if "drill" in title: type="drills"
+        if len(title)>5:
+            lesson={'id':id, 'month':month, 'stroke':stroke,'name':title, 'type':type }
+            #print lesson
+            lessons.append(lesson)
+            id+=1
         
-        
-        
+    out = open("sot.json","w")   
+    for l in lessons: 
+        pprint(l)    
+        out.write(json.dumps(l)+"\n")
+    out.close()
         
 
 if __name__ == "__main__":
